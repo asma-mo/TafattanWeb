ac610ea8f9b381b0859ed02ca563cc874e0aacfd
asma <Asma.mo@outlook.com>
2025-03-11 04:33:56 +0300

    Add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { Analytics } from '@vercel/analytics/next';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
